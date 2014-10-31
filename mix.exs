6c3b3bf477dc66a0ce5edac6686b43e7f84120ac
Michael Truog <mjtruog@gmail.com>
2014-10-30 18:39:41 -0700

    Update hex package.

---
+++ mix.exs
@@ -0,0 +1,30 @@
+defmodule CloudIServiceFilesystem do
+  use Mix.Project
+
+  def project do
+    [app: :cloudi_service_filesystem,
+     version: "1.4.0-rc.4",
+     language: :erlang,
+     description: description,
+     package: package,
+     deps: deps]
+  end
+
+  defp deps do
+    [{:cloudi_core, "~> 1.4.0-rc.4"},
+     {:cowlib, "~> 1.0.0"}]
+  end
+
+  defp description do
+    "Erlang/Elixir Cloud Framework Filesystem Service"
+  end
+
+  defp package do
+    [files: ~w(src doc test rebar.config README.markdown),
+     contributors: ["Michael Truog"],
+     licenses: ["BSD"],
+     links: %{"Website" => "http://cloudi.org",
+              "GitHub" => "https://github.com/CloudI/" <>
+                          "cloudi_service_db_filesystem"}]
+   end
+end
