22aa2b113a1533eaa7adf6ab0d7a80ab4a7840b4
Michael Truog <mjtruog@gmail.com>
2017-01-10 18:21:48 -0800

    Version 1.6.0.

--- mix.exs
+++ mix.exs
@@ -3,15 +3,15 @@ defmodule CloudIServiceFilesystem do
 
   def project do
     [app: :cloudi_service_filesystem,
-     version: "1.5.4",
+     version: "1.6.0",
      language: :erlang,
-     description: description,
-     package: package,
-     deps: deps]
+     description: description(),
+     package: package(),
+     deps: deps()]
   end
 
   defp deps do
-    [{:cloudi_core, "~> 1.5.4"},
+    [{:cloudi_core, "~> 1.6.0"},
      {:cowlib, "~> 1.0.0"}]
   end
 
