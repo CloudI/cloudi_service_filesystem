2335755f1cd61db7fb266d6cef1097b2efb46d8d
Michael Truog <mjtruog@gmail.com>
2017-03-28 20:20:40 -0700

    Version 1.7.0.

--- mix.exs
+++ mix.exs
@@ -1,9 +1,12 @@
+#-*-Mode:elixir;coding:utf-8;tab-width:2;c-basic-offset:2;indent-tabs-mode:()-*-
+# ex: set ft=elixir fenc=utf-8 sts=2 ts=2 sw=2 et nomod:
+
 defmodule CloudIServiceFilesystem do
   use Mix.Project
 
   def project do
     [app: :cloudi_service_filesystem,
-     version: "1.6.0",
+     version: "1.7.0",
      language: :erlang,
      description: description(),
      package: package(),
@@ -11,7 +14,7 @@ defmodule CloudIServiceFilesystem do
   end
 
   defp deps do
-    [{:cloudi_core, "~> 1.6.0"},
+    [{:cloudi_core, "~> 1.7.0"},
      {:cowlib, "~> 1.0.0"}]
   end
 
