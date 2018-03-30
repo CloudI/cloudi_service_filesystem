4dab199218b47a6c57f283580f140a3ef825cefd
Michael Truog <mjtruog@protonmail.com>
2018-03-29 17:40:32 -0700

    Switch cloudi.org links from http to https.

--- mix.exs
+++ mix.exs
@@ -26,7 +26,7 @@ defmodule CloudIServiceFilesystem do
     [files: ~w(src doc test rebar.config README.markdown),
      maintainers: ["Michael Truog"],
      licenses: ["MIT"],
-     links: %{"Website" => "http://cloudi.org",
+     links: %{"Website" => "https://cloudi.org",
               "GitHub" => "https://github.com/CloudI/" <>
                           "cloudi_service_db_filesystem"}]
    end
