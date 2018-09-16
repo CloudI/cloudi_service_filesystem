051996a3e5322e1eb14462a23356812f443e1405
Michael Truog <mjtruog@protonmail.com>
2018-09-15 20:44:57 -0700

    Version 1.7.4.

--- mix.exs
+++ mix.exs
@@ -6,15 +6,30 @@ defmodule CloudIServiceFilesystem do
 
   def project do
     [app: :cloudi_service_filesystem,
-     version: "1.7.3",
+     version: "1.7.4",
      language: :erlang,
+     erlc_options: [
+       :debug_info,
+       :warnings_as_errors,
+       :strict_validation,
+       :warn_bif_clash,
+       :warn_deprecated_function,
+       :warn_export_all,
+       :warn_export_vars,
+       :warn_exported_vars,
+       :warn_obsolete_guard,
+       :warn_shadow_vars,
+       :warn_unused_import,
+       :warn_unused_function,
+       :warn_unused_record,
+       :warn_unused_vars],
      description: description(),
      package: package(),
      deps: deps()]
   end
 
   defp deps do
-    [{:cloudi_core, "~> 1.7.3"},
+    [{:cloudi_core, "~> 1.7.4"},
      {:cowlib, "~> 1.0.0"}]
   end
 
