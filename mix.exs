f8f4e5c0dc62903cab5f27432d8409b4d9fa7f61
Michael Truog <mjtruog@protonmail.com>
2020-06-03 19:53:58 -0700

    Version 2.0.0.

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule CloudIServiceFilesystem do
 
   def project do
     [app: :cloudi_service_filesystem,
-     version: "1.8.0",
+     version: "2.0.0",
      language: :erlang,
      erlc_options: [
        :debug_info,
@@ -29,7 +29,7 @@ defmodule CloudIServiceFilesystem do
   end
 
   defp deps do
-    [{:cloudi_core, "~> 1.8.0"},
+    [{:cloudi_core, "~> 2.0.0"},
      {:cowlib, "~> 1.0.2"}]
   end
 
